8a025c548c054a037ee3b84e960e6377bc1957e6
GamerBlue208 <115688181+GamerBlue208@users.noreply.github.com>
2023-06-02 09:32:20 +0200

    a

--- resources/main.js
+++ resources/main.js
@@ -7,34 +7,3 @@ document.addEventListener('scroll', function() {
         header.style.background = "none";
     }
 });
-
-
-window.addEventListener('load', function() {
-    const content = document.querySelector('#content');
-    const contentHeight = content.offsetHeight;
-  
-    const hr = document.querySelector('#page_bottom');
-    let remainingHeight = Math.floor(100 - contentHeight);
-    
-    if (remainingHeight < 0) {
-        // If remaining height is negative, set it to 0
-        remainingHeight = 0;
-    }
-  
-    hr.style.marginBottom = `${remainingHeight}vh`;
-});
-
-window.addEventListener('resize', function() {
-    const content = document.querySelector('#content');
-    const contentHeight = content.offsetHeight;
-  
-    const hr = document.querySelector('#page_bottom');
-    let remainingHeight = Math.floor(100 - contentHeight);
-    
-    if (remainingHeight < 0) {
-        // If remaining height is negative, set it to 0
-        remainingHeight = 0;
-    }
-  
-    hr.style.marginBottom = `${remainingHeight}vh`;
-});
